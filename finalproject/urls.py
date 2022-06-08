f432e6aa3d857cbba881e593c9fdaa73a846daae
conradoga <conradocitybell@gmail.com>
2022-06-08 19:53:25 -0300

    models

--- finalproject/urls.py
+++ finalproject/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from restaurante import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("restaurante/",views.mostrarcomida, name = "restaurante")
 ]
--- restaurante/models.py
+++ restaurante/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class hamburguesas(models.Model):
+    nombre = models.CharField(max_length= 30)
+    ingredientes = models.CharField(max_length= 15)
+    descripcion = models.CharField(max_length= 40)
+    precio = models.IntegerField(max_length= 5)
+class ensaladas(models.Model):
+    nombre = models.CharField(max_length= 30)
+    ingredientes = models.CharField(max_length= 15)
+    descripcion = models.CharField(max_length= 40)
+    precio = models.IntegerField(max_length= 5)
+class bebidas(models.Model):
+    nombre = models.CharField(max_length= 30)
+    descripcion = models.CharField(max_length= 40)
+    precio = models.IntegerField(max_length= 5)
+
+
